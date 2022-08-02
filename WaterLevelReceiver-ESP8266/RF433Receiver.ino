e47cae23200548defc7f6a42b84623d6c102cd4d
Mamunul Mazid <mamunul.mazid@gmail.com>
2022-08-02 22:09:24 +0600

    create a new file for rf433 module code

---
+++ WaterLevelReceiver-ESP8266/RF433Receiver.ino
@@ -0,0 +1,30 @@
+#include <RH_ASK.h>
+#ifdef RH_HAVE_HARDWARE_SPI
+#include <SPI.h>  // Not actually used but needed to compile
+#endif
+
+// RH_ASK driver;
+RH_ASK driver(2000, 2, 4, 5);  // ESP8266 or ESP32: do not use pin 11 or 2
+/// RH_ASK driver(2000, 2, 4, 5);
+/// \endcode
+/// Which will initialise the driver at 2000 bps, recieve on GPIO2, transmit on GPIO4, PTT on GPIO5.
+/// Caution: on the tronixlabs breakout board, pins 4 and 5 may be labelled vice-versa.
+
+
+void setup_receiver() {
+  driver.init();
+}
+
+void receive() {
+  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+  uint8_t buflen = sizeof(buf);
+  if (driver.recv(buf, &buflen))  // Non-blocking
+  {
+    Serial.print("received:");
+    for (int i = 0; i < buflen; i++) {
+      Serial.print(char(buf[i]));
+    }
+    Serial.println();
+  } else {
+  }
+}
