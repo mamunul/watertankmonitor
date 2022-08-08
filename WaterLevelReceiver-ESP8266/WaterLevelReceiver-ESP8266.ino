ff4d9d77846ccdbe78eac35d64678ede62a08298
MamunulMazid <mamunul.mazid@gmail.com>
2022-08-08 17:49:40 +0600

    add homebridge test code

--- WaterLevelReceiver-ESP8266/WaterLevelReceiver-ESP8266.ino
+++ WaterLevelReceiver-ESP8266/WaterLevelReceiver-ESP8266.ino
@@ -1,10 +1,12 @@
 void setup() {
   Serial.begin(9600);  // Debugging only
-  setup_receiver();
+  // setup_receiver();
   // setup_receiver_rf24();
+  mqtt_setup();
 }
 
 void loop() {
-  receive();
+  // receive();
   // receive_rf24();
+  mqtt_loop();
 }
