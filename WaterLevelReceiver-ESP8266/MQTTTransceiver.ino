7e4fe17a826eb75406fc43665ec5a3dde5f40673
Mamunul Mazid <mamunul.mazid@gmail.com>
2022-08-10 20:27:33 +0600

    subscribe a topic

--- WaterLevelReceiver-ESP8266/MQTTTransceiver.ino
+++ WaterLevelReceiver-ESP8266/MQTTTransceiver.ino
@@ -1,35 +1,11 @@
-/*
- Basic ESP8266 MQTT example
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
-*/
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-// Update these with values suitable for your network.
-
 const char* ssid = "Trekkers_Hut";
 const char* password = "Alcrai.Ace1992";
-// const char* mqtt_server = "192.168.1.234";
 const char* mqtt_server = "raspberrypi.local";
-// int server_port =  8581;
 int server_port = 1883;
-// int server_port = 51745;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -40,7 +16,6 @@ int value = 0;
 void setup_wifi() {
 
   delay(10);
-  // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -61,27 +36,29 @@ void setup_wifi() {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived on topic: ");
   Serial.print(topic);
-  Serial.print(". Message: ");
-  String messageTemp;
+  String message;
   
   for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-    messageTemp += (char)payload[i];
+    message += (char)payload[i];
   }
-  Serial.println();
+  Serial.print(":");
+  Serial.println(message);
 }
 
 void reconnect() {
   while (!client.connected()) {
-    byte mac[5];  // create client ID from mac address
-    WiFi.macAddress(mac);
     Serial.println("Attempting MQTT connection...");
     String clientId = "ESPClient8266";
+      clientId += String(random(0xffff), HEX);
+
+  WiFi.mode(WIFI_STA);
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
-      client.subscribe("homebridge/from/topics/geton");
+      client.publish("topics/startup", "starting");
+      bool status = client.subscribe("topics/seton");
+      Serial.print("subscribed:");
+      Serial.println(status);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -94,11 +71,9 @@ void mqtt_setup() {
   setup_wifi();
   client.setServer(mqtt_server, server_port);
   client.setCallback(callback);
-  delay(1000);
 }
 
 void mqtt_loop() {
-
   if (!client.connected()) {
     reconnect();
   }
